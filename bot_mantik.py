8fdad875a4fce8389c3114f232e0e9743e2a1b8e
dAta <142596772+duruozgur@users.noreply.github.com>
2023-09-04 21:13:32 +0300

    Create bot_mantik.py

---
+++ bot_mantik.py
@@ -0,0 +1,28 @@
+#from bot_mantik import gen_pass
+
+#gen_pass(10)
+
+import random
+
+def sifre_olusturucu(sifre_uzunlugu):
+    ogeler = "+-/*!&amp;$#?=@<>"
+    sifre = ""
+
+    for i in range(sifre_uzunlugu):
+        sifre += random.choice(ogeler)
+
+    return sifre
+
+def emoji_olusturucu():
+    emoji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emoji)
+
+def yazi_tura():
+    para = random.randint(0, 2)
+    if para == 0:
+        return "YAZI"
+    else:
+        return "TURA"
+
+
+}
