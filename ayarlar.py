3d9174def5189c0df9bda95c4a86581d047aaa5b
dAta <142596772+duruozgur@users.noreply.github.com>
2023-09-04 21:06:43 +0300

    Create ayarlar.py

---
+++ ayarlar.py
@@ -0,0 +1,5 @@
+#settings.py in original
+
+ayarlar = {
+    "on_taki": ">", #ön takı, prefix (maybe it won't work if we call it different here)
+    "TOKEN": "MTE0NDcxNjY2NDExODcxODU1NA.GDi5th.1LZUk8LdruhHvS8CrrYORDVUzAEm_3Pk6ZccQk"
